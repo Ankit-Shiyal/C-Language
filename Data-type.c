5aed4e360a3438366d8978acbc73d225f8ab90d6
Ankit-Shiyal <ankitshiyal2005@gmail.com>
2025-10-06 16:31:01 +0530

    Add files via upload

---
+++ Data-type.c
@@ -0,0 +1,20 @@
+
+#include <stdio.h>
+
+    int main(){
+
+        int number=50;
+
+        printf("number:%d", number);
+
+        float pi=3.144444444;
+        printf("\npi Value:%f",pi);
+
+        char character='a';
+        printf("\nchar:%c",character);
+
+        char* string="ankit";
+        printf("\nstring:%s",string);   
+
+        return 0;
+    }
