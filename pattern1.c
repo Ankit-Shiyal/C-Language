9831f0ab697e6d6710590d59513fa0974d75d701
Ankit-Shiyal <ankitshiyal2005@gmail.com>
2025-10-06 16:27:36 +0530

    Add files via upload

---
+++ pattern1.c
@@ -0,0 +1,16 @@
+
+
+#include <stdio.h>
+
+int main() {
+
+    printf("- - - - - - - -");
+    printf("\n|" "\t \t \t  |");
+    printf("\nR" "\t \t \t  |");
+    printf("\nN" "\t \t \t  |");
+    printf("\nW" "\t \t \t  |");
+    printf("\n|" "\t \t \t  |");
+    printf("\n- - - - - - - -");
+
+    return 0;
+}
---
+++ pattern4.c
@@ -0,0 +1,17 @@
+
+#include <stdio.h>
+
+    int main() {
+
+        printf("* \t \t \t \t * \t \t \t \t * \t \t \t \t *");
+        printf("\n");
+        printf("\n \t * \t \t* \t \t * \t \t * \t \t * \t \t*");
+        printf("\n");
+        printf("\n \t \t* \t \t \t \t * \t \t \t \t *");
+        printf("\n");
+        printf("\n \t * \t \t* \t \t * \t \t * \t \t * \t \t *");
+        printf("\n");
+        printf("\n* \t \t \t \t * \t \t \t \t * \t \t \t \t *");
+        
+        return 0;
+    }
