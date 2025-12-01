f175263cc3a5fa546a5f8f7daea637a1663e7489
Ankit-Shiyal <ankitshiyal2005@gmail.com>
2025-12-01 16:37:41 +0530

    Add files via upload

---
+++ ch-12_string/1.char.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(){
+
+
+    char str[6]="hello";
+
+    printf("%s\n",str);
+
+    char greeting[] = {'g','o','o','d','m','o','r','n','i','n','g','\0'};
+
+    printf("%s\n",greeting);
+
+
+    char latter='h';
+
+    
+    printf("%d\n",latter);
+    
+    printf("%c\n",latter);
+
+
+    return 0;
+}
---
+++ ch-12_string/3.updation.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(){
+
+    // static
+
+    char letter[6] ="hello";
+    
+    letter[1]='a';
+
+    printf("%s\n",letter);
+
+
+    // dynamic
+
+    printf("enter character to change\n");
+    scanf("%c",&letter[4]);
+
+     printf("%s",letter);
+
+
+    return 0;
+
+
+}
