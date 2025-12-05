b5ac4502b990c1722953f846c4908741af18b4ca
Ankit-Shiyal <ankitshiyal2005@gmail.com>
2025-12-05 15:08:33 +0530

    Add files via upload

---
+++ 13-function/5.nested.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void bankBalance (){
+
+    printf("check bank balance.. \n");
+}
+
+void withDraw(){
+
+    bankBalance ();
+    printf("mony transfer successfully");
+
+}
+
+int main(){
+
+    withDraw();
+
+    return 0;
+}
